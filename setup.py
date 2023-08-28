40debe815e50e4c2d20dfb57e6f80ded121efa8c
tushar2704 <tushar.inseec@gmail.com>
2023-08-29 02:16:19 +0530

    Project Structure updates

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "1.0.0"
+
+REPO_NAME = "Company_Bankruptcy_Prediction"
+AUTHOR_USER_NAME = "tushar2704"
+SRC_REPO = "CB"
+AUTHOR_EMAIL = "tushar.27041994@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Predicting company bankruptcy using machine learning",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
